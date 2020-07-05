a612693f3c6746d93f1211717b6779cf22a2ab34
Nagmet <60585133+Nagmet@users.noreply.github.com>
2020-07-05 19:07:26 +0300

    Поехали!

---
+++ js/script.js
@@ -0,0 +1,27 @@
+
+function testWebP(callback) {
+    var webP = new Image(); 
+    webP.onload = webP.onerror = function () { 
+        callback(webP.height == 2); };
+        webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+    if (support == true) { 
+        document.querySelector('body').classList.add('webp'); 
+    }else{ document.querySelector('body').classList.add('no-webp'); 
+}
+});
+
+
+
+$(document).ready(function() {
+    $('.header__navbar-burger').click(function (event) {
+        $('.header__navbar-burger, .header__navbar-nav').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
+
+$(document).ready(function(){
+    $('.slider').slick();
+});
